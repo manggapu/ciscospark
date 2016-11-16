7af48c4ad579e339bbe527f14ab5f2526fddcb8b
manggapu <clivexs@gmail.com>
2016-11-16 12:57:48 +1100

    Updated 'create'

--- ciscospark/webhooks.py
+++ ciscospark/webhooks.py
@@ -26,7 +26,7 @@ def get_details(token, webhookId):
 # POST Requests
 def create(token, name, targetUrl, resource, event, secret, filter=''):
     headers = {'Authorization': common._sparktoken(token), 'content-type': 'application/json'}
-    payload = {'name': name, 'targetUrl': targetUrl, 'resource': resource, 'event': event, 'secret': secret, 'filter': filter}
+    payload = {'name': name, 'targetUrl': targetUrl, 'resource': resource, 'event': event, 'secret': secret}
     if filter: payload['filter'] = filter 
     resp = requests.post(url=common._sparkurl('/webhooks'), json=payload, headers=headers)
     webhook_dict = json.loads(resp.text)
